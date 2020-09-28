366c89d63848f947924297a022a559d1998605e8
yaqoobjunejo <58535935+yaqoobjunejo@users.noreply.github.com>
2020-09-28 23:16:38 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -20,20 +20,19 @@ for (var i=0; themeDots.length > i; i++){
 }
 
 function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
-
 	if(mode == 'blue'){
 		document.getElementById('theme-style').href = 'blue.css'
 	}
-
 	if(mode == 'green'){
 		document.getElementById('theme-style').href = 'green.css'
 	}
 
 	if(mode == 'purple'){
 		document.getElementById('theme-style').href = 'purple.css'
+	}
+
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'light.css'
 	}
 
 	localStorage.setItem('theme', mode)
